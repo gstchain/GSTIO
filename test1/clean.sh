968bbe38679560dc50637287578d8a24e5dd3507
a6lian <45474514+a6lian@users.noreply.github.com>
2019-07-11 13:50:31 +0800

    releases

--- test1/clean.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-rm -rf  /work/gst_install/test1/data/*
-
-
-
--- test1/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-
-
-#nohup nodgst --data-dir ./ --config-dir ./  --genesis-json=genesis.json --max-transaction-time=3000 > /work/gst_install/test1/bpnode.log 2>&1 &
-
-nodgst --data-dir ./data/ --config-dir ./
