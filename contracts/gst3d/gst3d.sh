b110de7f2e595f2178ed3afa6cfcdc937bd50389
gstchain <61673197+gstchain@users.noreply.github.com>
2020-03-02 20:38:20 +0800

    gstchain
    Update all

--- contracts/gst3d/gst3d.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd /work/gst_install/
-
-clgst set contract vbk4pgci3vgk contracts/gst3d -p vbk4pgci3vgk
-
-
- clgst set account permission vbk4pgci3vgk active '{"threshold": 1,"keys": [{"key":"GST5nLfQAVBi1stzgXZNqnzsQ1gDZ271rry2vJF5jMHv2Qb8nYXf3", "weight":1}],"accounts":     [{"permission":{"actor":"vbk4pgci3vgk","permission":"gstio.code"},"weight":1}]}' owner -p  vbk4pgci3vgk
--- contracts/pokedicegame/pokedicelogs/pokedicelogs.cpp
+++
@@ -1,6 +0,0 @@
-#include "pokedicelogs.hpp"
-
-void pokedicelogs::result(st_result result) {
-    require_auth(FAIR_DICE_GAME);
-    require_recipient(result.player);
-}
